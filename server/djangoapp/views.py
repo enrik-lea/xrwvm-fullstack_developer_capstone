47cb9623992f9f5fdfa52a8bbb719e34d4cf3a5c
enrik-lea <63104296+enrik-lea@users.noreply.github.com>
2024-08-31 19:01:13 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -30,8 +30,8 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name,
-            "CarMake": car_model.car_make.name})
+        cars.append(
+            {"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
 
 
@@ -86,13 +86,14 @@ def registration(request):
         # Create user in auth_user table
         user = User.objects.create_user(username=username,
                                         first_name=first_name,
-                                        last_name=last_name,password=password,
+                                        last_name=last_name,
+                                        password=password,
                                         email=email)
         # Login the user and redirect to list page
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
-    else :
+    else:
         data = {"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
